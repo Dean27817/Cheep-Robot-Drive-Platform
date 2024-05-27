a00702e4e7a59ebbe7b8935ba08842bf6f55f957
Dean Morgan <79397523+Dean27817@users.noreply.github.com>
2024-05-27 12:23:44 -0600

    Added code to make the drive actually work
    After too long I found a library that will actually work with these speed controllers. The drive code is in a separate function from the dabble code. just makes it a bit cleaner. Still need to finish the docs, but it works.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 //this code is designed to be used on a esp32-S3-Zero from waveshare
 //it uses a basic 2 chanel H-Bridge motor controller, each chanel driving 2 motors with custom printed gearboxes
 
-//defonitions to use the gamepad modual on dabble
+//used to see if the program is running on an esp32 board
+#if !defined( ESP32 ) 
+  #error This code is intended to run on the ESP32 platform! Please check your Tools->Board setting. 
+#endif 
+
+//defonitions to use the gamepad moduel on dabble
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 
@@ -11,93 +16,81 @@
 #include <Arduino.h>
 //used to allow the user to control the robot with their phone
 #include <DabbleESP32.h>
-//used to control PWM for the MC
-#include <ESP32Servo.h>
-//Used to make the speed controller easier to work with
-#include "ESC.h"
-
-
+//used for PWM
+#include <ESP32PWM.h>
 //define the pins that the inputs of the Motor controller will be solderd to
 #define in1 5
 #define in2 3
 #define in3 4
-#define in4 2
+#define in4 10
 
-//defines the limits of the speeds being sent
-#define MIN_SPEED 0 // speed just slow enough to turn motor off
-#define MAX_SPEED 2000 // speed where my motor drew 3.6 amps at 12v.
+//creates the objects from the PWM library
+ESP32PWM speedControl1;
+ESP32PWM speedControl2;
+ESP32PWM speedControl3;
+ESP32PWM speedControl4;
 
-//declaring an ESC object for each "in" pin
-ESC Speed1(in1, 0, 3000, 500);
-ESC Speed2(in2, 0, 3000, 500);
-ESC Speed3(in3, 0, 3000, 500);
-ESC Speed4(in4, 0, 3000, 500);
+//the maximum frequency of each pin
+int freq = 1000;
 
 void setup() {
-    //used for communication with the computer
-    Serial.begin(9600);
-    //starts the bluetooth connection
-    Dabble.begin("Dean Morgan Drive Base");
-    delay(1000);
+  // Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
 
-    //Allow the pins to send electricity out
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
+  //used for communication with the computer
+  Serial.begin(9600);
 
-    //prepares the pins for PWM
-    Speed1.arm();
-    Speed2.arm();
-    Speed3.arm();
-    Speed4.arm();
+  //starts the bluetooth connection
+  Dabble.begin("Dean Morgan Drive Base");
+
+  //tells the code wich pins are being used for each clock
+  speedControl1.attachPin(in1, freq, 10);
+  speedControl2.attachPin(in2, freq, 10);
+  speedControl3.attachPin(in3, freq, 10);
+  speedControl4.attachPin(in4, freq, 10);
 }
+
+
+void drive(float left, float right)
+{
+  //controls the left motors
+  //each motor will take a value between 0 and 1
+  if(left > 0)
+  {
+    speedControl1.writeScaled(abs(left));
+    speedControl2.writeScaled(0);
+  }
+  else
+  {
+    speedControl1.writeScaled(0);
+    speedControl2.writeScaled(abs(left));
+  }
+
+  //controls the right motors
+  //each motor will take a value between 0 and 1
+  if(right > 0)
+  {
+    speedControl3.writeScaled(abs(right));
+    speedControl4.writeScaled(0);
+  }
+  else
+  {
+    speedControl3.writeScaled(0);
+    speedControl4.writeScaled(abs(right));
+  }
+}
+
+
 
 void loop() {
   //makes the esp32 reload the input from the phone
   Dabble.processInput();
 
-  //gets the speed values that the left and right wheels should be traveling
-  //the multiplication is to take the -7 to 7 value given by dabble and turns it into a usable value to use in the ESC functions
-  double Left = (GamePad.getYaxisData() - GamePad.getXaxisData()) * (MAX_SPEED/7);
-  double Right = (GamePad.getYaxisData() - GamePad.getXaxisData()) * (MAX_SPEED/7);
+  float forward = GamePad.getYaxisData()/7;
+  float rotate = GamePad.getXaxisData()/7;
 
-  //right wheel forward
-  if(Right > 0.2)
-  {
-    digitalWrite(in1, LOW);
-    Speed2.speed(abs(Right));
-  }
-  //right wheel stop
-  else if(Right > -0.2 && Right < 0.2)
-  {
-    Speed1.stop();
-    Speed2.stop();
-  }
-  //right wheel backwards
-  else 
-  {
-    digitalWrite(in2, LOW);
-    Speed1.speed(abs(Right));
-  }
-
-
-  //left wheel forward
-  if(Left > 0.2)
-  {
-    digitalWrite(in3, LOW);
-    Speed4.speed(abs(Left));
-  }
-  //left wheel stop
-  else if(Left > -0.2 && Left < 0.2)
-  {
-    Speed3.stop();
-    Speed4.stop();
-  }
-  //left wheel backwards
-  else 
-  {
-    digitalWrite(in4, LOW);
-    Speed3.speed(abs(Left));
-  }
+  drive(forward - rotate, forward + rotate);
 }
