a6687cbc6b54486768208c3691c6911fce3ffc34
Dean Morgan <79397523+Dean27817@users.noreply.github.com>
2024-08-24 20:43:06 -0600

    added different drive modes

--- main.cpp
+++ main.cpp
@@ -1,43 +1,22 @@
-/*
-  Cheep robot base
-    > created by Dean Morgan
-  
-  This base code was designed with the following hardware in mind:
-    > Waveshare esp32-S3-zero
-    > 2 channel H-Bridge motor controller
-    > Maximum of 2 motors
-      > one left motor and one right motor
-*/
-
-//used to see if the program is running on an esp32 board
-#if !defined( ESP32 ) 
-  #error This code is intended to run on the ESP32 platform! Please check your Tools->Board setting. 
-#endif 
-
-//definitions to use the game-pad module on dabble
-#define CUSTOM_SETTINGS
-#define INCLUDE_GAMEPAD_MODULE
-
-//basic arduino library
-#include <Arduino.h>
-//used to allow the user to control the robot with their phone
-#include <DabbleESP32.h>
-//used for PWM
-#include <ESP32PWM.h>
+#include "Arduino.h"
+#include "ESP32Servo.h"
+#include <XboxSeriesXControllerESP32_asukiaaa.hpp>
 //define the pins that the inputs of the Motor controller will be soldered to
-#define in1 6
-#define in2 5
-#define in3 8
-#define in4 7
+#define in1 10
+#define in2 8
+#define in3 3
+#define in4 5
 
 //creates the objects from the PWM library
 ESP32PWM speedControl1;
 ESP32PWM speedControl2;
 ESP32PWM speedControl3;
 ESP32PWM speedControl4;
+XboxSeriesXControllerESP32_asukiaaa::Core xboxController;
 
 //the maximum frequency of each pin
 int freq = 1000;
+int controllerMode = 0;
 
 float forward;
 float rotate;
@@ -50,33 +29,37 @@ void drive(float left, float right)
   //makes it so that if one is soldered backwards you dont have to worry about it because you can just reverse one of the motors
   left *= -1; 
   right *= -1;
+
+  float buffer = 0.1;
   //controls the left motors
   //each motor will take a value between 0 and 1
-  if(left > 0)
+  if(left > buffer || left < -buffer || right > buffer || right < -buffer)
   {
-    speedControl1.writeScaled(abs(left));
-    speedControl2.writeScaled(0);
+    if(left > 0)
+    {
+      speedControl1.writeScaled(abs(left));
+      speedControl2.writeScaled(0);
+    }
+    else
+    {
+      speedControl1.writeScaled(0);
+      speedControl2.writeScaled(abs(left));
+    }
+
+    //controls the right motors
+    //each motor will take a value between 0 and 1
+    if(right > 0)
+    {
+      speedControl3.writeScaled(abs(right));
+      speedControl4.writeScaled(0);
+    }
+    else
+    {
+      speedControl3.writeScaled(0);
+      speedControl4.writeScaled(abs(right));
+    }
   }
   else
-  {
-    speedControl1.writeScaled(0);
-    speedControl2.writeScaled(abs(left));
-  }
-
-  //controls the right motors
-  //each motor will take a value between 0 and 1
-  if(right > 0)
-  {
-    speedControl3.writeScaled(abs(right));
-    speedControl4.writeScaled(0);
-  }
-  else
-  {
-    speedControl3.writeScaled(0);
-    speedControl4.writeScaled(abs(right));
-  }
-
-  if(right == 0 && left == 0)
   {
     speedControl1.writeScaled(0);
     speedControl2.writeScaled(0);
@@ -96,8 +79,7 @@ void setup() {
   Serial.begin(9600);
 
   //starts the bluetooth connection
-  Dabble.begin("Cheep Chirp Speed");
-
+  xboxController.begin();
   //tells the code which pins are being used for each clock
   speedControl1.attachPin(in1, freq, 10);
   speedControl2.attachPin(in2, freq, 10);
@@ -108,7 +90,6 @@ void setup() {
   drive(1, -1);
   delay(50);
   drive(0, 0);
-  Dabble.waitForAppConnection();
   drive(1, -1);
   delay(50);
   drive(-1, 1);
@@ -116,23 +97,56 @@ void setup() {
   drive(0, 0);
 }
 
-void loop() {
-  //Makes the ESP32 reload the input from the phone
-  Dabble.processInput();
+void loop()
+{
+   //refreshes the input from the xbox
+  xboxController.onLoop();
 
-  //Check if the device is connected
-  if (!esp32ble.available()) {
-    // If connected, allow the robot to move
-      forward = -1 * ((0.002915451895044) * (GamePad.getYaxisData() * GamePad.getYaxisData() * GamePad.getYaxisData()));
-      rotate = -1 * (GamePad.getXaxisData()/10);  
-    drive(rotate - forward, rotate + forward);
-  } else {
-    //If not connected, stop the robot
-    drive(0, 0);
-    //add a message to indicate disconnection
-    Serial.println("App disconnected. Stopping the robot.");
+  //checks if th3e controler is connected
+  if(xboxController.isConnected())
+  {
+    //checks if the controler has been fully set up
+    if(!xboxController.isWaitingForFirstNotification())
+    {
+        float left = ((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2;
+        float right = ((xboxController.xboxNotif.joyLVert - 32767.5) / (65535))*2;
+
+        Serial.println(xboxController.xboxNotif.joyRVert);
+
+        switch(controllerMode)
+        {
+          case 0:
+          drive(0, 0);
+          break;
+          case 1:
+          drive(((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2, ((xboxController.xboxNotif.joyLVert - 32767.5) / (65535))*2);
+          break;
+          case 2:
+          drive((((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) + (((xboxController.xboxNotif.joyRHori - 32767.5) / (65535))*2), (((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) - (((xboxController.xboxNotif.joyRHori - 32767.5) / (65535))*2));
+          break;
+          case 3:
+          drive((((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) + (((xboxController.xboxNotif.joyLHori - 32767.5) / (65535))*2), (((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) - (((xboxController.xboxNotif.joyLHori - 32767.5) / (65535))*2));
+          break;
+        };
+      }
+
+      
+
+      if(xboxController.xboxNotif.btnA)
+      {
+        controllerMode = 1;
+      }
+      else if (xboxController.xboxNotif.btnB)
+      {
+        controllerMode = 2;
+      }
+      else if (xboxController.xboxNotif.btnX)
+      {
+        controllerMode = 0;
+      }
+      else if (xboxController.xboxNotif.btnY)
+      {
+        controllerMode = 3;
+      }
   }
-  
-  //Continue to wait for app connection in the loop
-  Dabble.waitForAppConnection();
 }
