f5293eb6f248da5cc995bee28c9dc821a4b765b1
Dean Morgan <79397523+Dean27817@users.noreply.github.com>
2024-09-29 09:41:35 -0600

    Accidentally Added xbox code
    The last code I sent was the xbox code instead of using dabble

--- main.cpp
+++ main.cpp
@@ -1,152 +1,104 @@
-#include "Arduino.h"
-#include "ESP32Servo.h"
-#include <XboxSeriesXControllerESP32_asukiaaa.hpp>
-//define the pins that the inputs of the Motor controller will be soldered to
-#define in1 10
-#define in2 8
-#define in3 3
-#define in4 5
+//cheep robot base
+//created by dean morgan
+//this code is designed to be used on a esp32-S3-Zero from waveshare
+//it uses a basic 2 chanel H-Bridge motor controller, each chanel driving 2 motors with custom printed gearboxes
 
-//creates the objects from the PWM library
-ESP32PWM speedControl1;
-ESP32PWM speedControl2;
-ESP32PWM speedControl3;
-ESP32PWM speedControl4;
-XboxSeriesXControllerESP32_asukiaaa::Core xboxController;
+//defonitions to use the gamepad modual on dabble
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
 
-//the maximum frequency of each pin
-int freq = 1000;
-int controllerMode = 0;
+//basic arduino library
+#include <Arduino.h>
+//used to allow the user to control the robot with their phone
+#include <DabbleESP32.h>
+//used to control PWM for the MC
+#include <ESP32Servo.h>
+//Used to make the speed controller easier to work with
+#include "ESC.h"
 
-float forward;
-float rotate;
 
-//used to make the code for the robot look cleaner
-//takes the speed the left and right motors should go (from -1 to 1) and works with the PWM to make them move appropriately
-void drive(float left, float right)
-{
-  //reverses the left or right motor
-  //makes it so that if one is soldered backwards you dont have to worry about it because you can just reverse one of the motors
-  left *= -1; 
-  right *= -1;
+//define the pins that the inputs of the Motor controller will be solderd to
+#define in1 5
+#define in2 3
+#define in3 4
+#define in4 2
 
-  float buffer = 0.1;
-  //controls the left motors
-  //each motor will take a value between 0 and 1
-  if(left > buffer || left < -buffer || right > buffer || right < -buffer)
-  {
-    if(left > 0)
-    {
-      speedControl1.writeScaled(abs(left));
-      speedControl2.writeScaled(0);
-    }
-    else
-    {
-      speedControl1.writeScaled(0);
-      speedControl2.writeScaled(abs(left));
-    }
+//defines the limits of the speeds being sent
+#define MIN_SPEED 0 // speed just slow enough to turn motor off
+#define MAX_SPEED 2000 // speed where my motor drew 3.6 amps at 12v.
 
-    //controls the right motors
-    //each motor will take a value between 0 and 1
-    if(right > 0)
-    {
-      speedControl3.writeScaled(abs(right));
-      speedControl4.writeScaled(0);
-    }
-    else
-    {
-      speedControl3.writeScaled(0);
-      speedControl4.writeScaled(abs(right));
-    }
-  }
-  else
-  {
-    speedControl1.writeScaled(0);
-    speedControl2.writeScaled(0);
-    speedControl3.writeScaled(0);
-    speedControl4.writeScaled(0);
-  }
-}
+//declaring an ESC object for each "in" pin
+ESC Speed1(in1, 0, 3000, 500);
+ESC Speed2(in2, 0, 3000, 500);
+ESC Speed3(in3, 0, 3000, 500);
+ESC Speed4(in4, 0, 3000, 500);
 
 void setup() {
-  // Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
+    //used for communication with the computer
+    Serial.begin(9600);
+    //starts the bluetooth connection
+    Dabble.begin("Dean Morgan Drive Base");
+    delay(1000);
 
-  //used for communication with the computer
-  Serial.begin(9600);
+    //Allow the pins to send electricity out
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
 
-  //starts the bluetooth connection
-  xboxController.begin();
-  //tells the code which pins are being used for each clock
-  speedControl1.attachPin(in1, freq, 10);
-  speedControl2.attachPin(in2, freq, 10);
-  speedControl3.attachPin(in3, freq, 10);
-  speedControl4.attachPin(in4, freq, 10);
-
-  //plays a startup on the drive motors
-  drive(1, -1);
-  delay(50);
-  drive(0, 0);
-  drive(1, -1);
-  delay(50);
-  drive(-1, 1);
-  delay(100);
-  drive(0, 0);
+    //prepares the pins for PWM
+    Speed1.arm();
+    Speed2.arm();
+    Speed3.arm();
+    //Speed4.arm();
 }
 
-void loop()
-{
-   //refreshes the input from the xbox
-  xboxController.onLoop();
+void loop() {
+  //makes the esp32 reload the input from the phone
+  Dabble.processInput();
 
-  //checks if th3e controler is connected
-  if(xboxController.isConnected())
+  //gets the speed values that the left and right wheels should be traveling
+  //the multiplication is to take the -7 to 7 value given by dabble and turns it into a usable value to use in the ESC functions
+  double Left = (GamePad.getYaxisData() - GamePad.getXaxisData()) * (MAX_SPEED/7);
+  double Right = (GamePad.getYaxisData() - GamePad.getXaxisData()) * (MAX_SPEED/7);
+
+  //right wheel forward
+  if(Right > 0.2)
   {
-    //checks if the controler has been fully set up
-    if(!xboxController.isWaitingForFirstNotification())
-    {
-        float left = ((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2;
-        float right = ((xboxController.xboxNotif.joyLVert - 32767.5) / (65535))*2;
-
-        Serial.println(xboxController.xboxNotif.joyRVert);
-
-        switch(controllerMode)
-        {
-          case 0:
-          drive(0, 0);
-          break;
-          case 1:
-          drive(((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2, ((xboxController.xboxNotif.joyLVert - 32767.5) / (65535))*2);
-          break;
-          case 2:
-          drive((((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) + (((xboxController.xboxNotif.joyRHori - 32767.5) / (65535))*2), (((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) - (((xboxController.xboxNotif.joyRHori - 32767.5) / (65535))*2));
-          break;
-          case 3:
-          drive((((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) + (((xboxController.xboxNotif.joyLHori - 32767.5) / (65535))*2), (((xboxController.xboxNotif.joyRVert - 32767.5) / (65535))*2) - (((xboxController.xboxNotif.joyLHori - 32767.5) / (65535))*2));
-          break;
-        };
-      }
-
-      
-
-      if(xboxController.xboxNotif.btnA)
-      {
-        controllerMode = 1;
-      }
-      else if (xboxController.xboxNotif.btnB)
-      {
-        controllerMode = 2;
-      }
-      else if (xboxController.xboxNotif.btnX)
-      {
-        controllerMode = 0;
-      }
-      else if (xboxController.xboxNotif.btnY)
-      {
-        controllerMode = 3;
-      }
+    digitalWrite(in1, LOW);
+    Speed2.speed(abs(Right));
   }
+  //right wheel stop
+  else if(Right > -0.2 && Right < 0.2)
+  {
+    Speed1.stop();
+    Speed2.stop();
+  }
+  //right wheel backwards
+  else 
+  {
+    digitalWrite(in2, LOW);
+    Speed1.speed(abs(Right));
+  }
+
+/*
+  //left wheel forward
+  if(Left > 0.2)
+  {
+    digitalWrite(in3, LOW);
+    Speed4.speed(abs(Left));
+  }
+  //left wheel stop
+  else if(Left > -0.2 && Left < 0.2)
+  {
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
+  //left wheel backwards
+  else 
+  {
+    digitalWrite(in4, LOW);
+    Speed3.speed(abs(Left));
+  }
+  */
 }
